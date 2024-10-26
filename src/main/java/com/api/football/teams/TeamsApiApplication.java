f2a73611cba00af67435522f50e0992b91e28586
João Paulo WSL <joao.almeida@wsl.com>
2024-10-26 07:40:29 -0300

    Initial Commit

---
+++ src/main/java/com/api/football/teams/TeamsApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.football.teams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/football/teams/TeamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.football.teams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
