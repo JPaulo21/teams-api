f955e4e19ba7e440fc52c2e4da3df78058e09ede
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2025-05-29 08:18:34 -0300

    feat: Adicionado @Builder para instanciar classes de forma dinâmica

--- src/main/java/com/api/football/teams/domain/Team.java
+++ src/main/java/com/api/football/teams/domain/Team.java
@@ -1,16 +1,14 @@
 package com.api.football.teams.domain;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "Teams")
