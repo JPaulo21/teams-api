99fb6c957adf2a00679418c7ed984899b26bcf09
João Paulo Lira de Almeida <joao.almeida@mv.com.br>
2025-05-30 13:59:54 -0300

    test: Adicionado teste para campos que não podem ser nulos

--- src/test/java/com/api/football/teams/domain/TeamRepositoryTest.java
+++ src/test/java/com/api/football/teams/domain/TeamRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.api.football.teams.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.stream.Stream;
 
 import static com.api.football.teams.common.TeamConstants.TEAM;
 import static org.assertj.core.api.Assertions.*;
@@ -33,6 +38,22 @@ public class TeamRepositoryTest {
         team.setId(null);
         assertThatCode(() ->teamRepository.save(team))
             .isInstanceOf(RuntimeException.class);
+    }
+
+    @ParameterizedTest
+    @MethodSource("providesInvalidTeams")
+    public void createTeam_withBlankDatas_throwsException(Team team){
+        assertThatThrownBy(() -> teamRepository.save(team))
+            .isInstanceOf(RuntimeException.class);
+    }
+
+    private static Stream<Arguments> providesInvalidTeams() {
+        return Stream.of(
+            Arguments.of(new Team()),
+            Arguments.of(new Team(null, null, null, null, null, null, null)),
+            Arguments.of(new Team("Sport Club do Recife", null, null, null, null, null, null)),
+            Arguments.of(new Team("Sport Club do Recife", "Sport Recife", null, null, null, null, null))
+        );
     }
 
 }
